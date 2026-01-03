8baebaeb9cd3bd54dcf0f67f0ac5ae3ce38bf12a
fahimjh <fahimjh001@gmail.com>
2026-01-03 23:08:49 +0600

    Initial server with mongodb connection setup

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
