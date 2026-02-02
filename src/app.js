3b872a1b2e6400ff713a87f11a9c84d8500b2ac1
fahimjh <fahimjh001@gmail.com>
2026-02-02 19:14:47 +0600

    Simplify CORS for dev, mount auth routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors());
+// Dev: allow any origin
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser());
 app.use(express.json());
 
+// Routes
+const authRoutes = require('./routes/authRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+const enrollmentRoutes = require('./routes/enrollmentRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
+const teacherApplicationRoutes = require('./routes/teacherApplicationRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('/api/enrollments', enrollmentRoutes);
+app.use('/api/teachers', teacherRoutes);
+app.use('/api/teacher-applications', teacherApplicationRoutes);
+
 app.get("/", (req, res) => {
-  res.send("ScholarX Backend API Running");
+  res.json({ message: "ScholarX Backend API Running" });
 });
 
 module.exports = app;
